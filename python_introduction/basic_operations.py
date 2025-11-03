fd034a036ee14073310007317999354fce005f7d
danabongo <danabongo104@gmail.com>
2025-11-03 20:55:59 +0300

    Add basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+# Define two variables with values 10 and 5 respectively
+number1 = 10
+number2 = 5 
+
+# Create a variable sum to hold the sum of variable1 and variable2
+sum = number1 + number2
+# Create a variable diff to hold the difference of variable1 and variable2
+diff = number1 - number2
+# Create a variable product to hold the product of variable1 and variable2.
+prod = number1 * number2
+
+#print results:
+print(f"Addition of 10 and 5 is {sum}")
+print(f"Subtraction of 10 and 5 is {diff}")
+print(f"Multiplication of 10 and 5 is {prod}")
+
