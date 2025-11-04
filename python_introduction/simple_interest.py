050cdf143f1613db4f3b04dc57b797972e3bd264
danabongo <danabongo104@gmail.com>
2025-11-04 17:08:43 +0300

    Add simple_interest file

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+# Define VAriables
+principal = 1000
+rate = 0.05
+time = 3
+# Calculate simple interest
+Simple_int = principal * time * rate
+# Print the result
+print(f"The simple interest is: {Simple_int}")
+
