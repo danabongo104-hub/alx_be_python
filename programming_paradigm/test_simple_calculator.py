357eca73ebaf6e92840beacea332afa2774d0b59
danabongo <danabongo104@gmail.com>
2025-12-01 15:42:43 +0300

    Fixed setUp var name

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,28 +6,28 @@ class TestsimpleCalculator(unittest.TestCase):
     def setUp(self):
         """set up the SimpleCalculator instance before each tests.
         """
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
 
     def test_addition(self):
-        self.assertEqual(self.calculator.add(2, 3), 5)
-        self.assertEqual(self.calculator.add(-1, 1), 0)
-        self.assertEqual(self.calculator.add(-1, -1), -2)
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
 
     def test_subtraction(self):
-        self.assertEqual(self.calculator.subtract(5, 3), 2)
-        self.assertEqual(self.calculator.subtract(-1, 1), -2)
-        self.assertEqual(self.calculator.subtract(-1, -1), 0)
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(-1, 1), -2)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
 
     def test_multiply(self):
-        self.assertEqual(self.calculator.multiply(2, 3), 6)
-        self.assertEqual(self.calculator.multiply(-1, 1), -1)
-        self.assertEqual(self.calculator.multiply(-1, -1), 1)
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(-1, -1), 1)
     
     def test_divide(self):
-        self.assertEqual(self.calculator.divide(6, 3), 2)
-        self.assertEqual(self.calculator.divide(-6, 2), -3)
-        self.assertEqual(self.calculator.divide(-6, -2), 3)
-        self.assertIsNone(self.calculator.divide(6, 0))
+        self.assertEqual(self.calc.divide(6, 3), 2)
+        self.assertEqual(self.calc.divide(-6, 2), -3)
+        self.assertEqual(self.calc.divide(-6, -2), 3)
+        self.assertIsNone(self.calc.divide(6, 0))
             
 
 if __name__ == "__main__":
