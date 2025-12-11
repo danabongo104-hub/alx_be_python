03b2909e00fc970d8fe39cfa15ad4d1b6e306df1
danabongo <danabongo104@gmail.com>
2025-12-11 15:27:50 +0300

    Add library_system and main.py

---
+++ oop/library_system.py
@@ -0,0 +1,39 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"'{self.title}' by {self.author}"
+
+class EBook(Book):
+    def __init__(self, title, author, file_size: int):
+        super().__init__(title, author)
+        self.file_size = file_size  # in KB
+
+    def __str__(self):
+        return f"'{self.title}' by {self.author}, File size: {self.file_size}KB"
+    
+class PrintBook(Book):
+    def __init__(self, title, author, page_count: int):
+        super().__init__(title, author)
+        self.page_count = page_count  # number of pages
+
+    def __str__(self):
+        return f"'{self.title}' by {self.author}, Page Count: {self.page_count}"
+    
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book: Book):
+        self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            if isinstance(book, EBook):
+                print(f"Ebook: {book}")
+            elif isinstance(book, PrintBook):
+                print(f"PrintBook: {book}")
+            else:
+                print(f"Book: {book}")
