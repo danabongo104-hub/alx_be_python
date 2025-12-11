af7e1fd47cf0e2878a2d6dbe6f7df17267a8329f
danabongo <danabongo104@gmail.com>
2025-12-11 15:31:28 +0300

    Updated str representation

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,7 +4,7 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return f"'{self.title}' by {self.author}"
+        return f"{self.title} by {self.author}"
 
 class EBook(Book):
     def __init__(self, title, author, file_size: int):
@@ -12,7 +12,7 @@ class EBook(Book):
         self.file_size = file_size  # in KB
 
     def __str__(self):
-        return f"'{self.title}' by {self.author}, File size: {self.file_size}KB"
+        return f"{self.title} by {self.author}, File size: {self.file_size}KB"
     
 class PrintBook(Book):
     def __init__(self, title, author, page_count: int):
@@ -20,7 +20,7 @@ class PrintBook(Book):
         self.page_count = page_count  # number of pages
 
     def __str__(self):
-        return f"'{self.title}' by {self.author}, Page Count: {self.page_count}"
+        return f"{self.title} by {self.author}, Page Count: {self.page_count}"
     
 class Library:
     def __init__(self):
